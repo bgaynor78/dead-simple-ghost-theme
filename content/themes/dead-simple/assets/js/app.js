2222de41e716493e822254684031b8f953610b29
Brett Gaynor <bgaynor@dresources.com>
2017-08-31 09:25:45 -0500

    Updates to work in progress. Migrated to 1.0.0

--- content/themes/dead-simple/assets/js/app.js
+++ content/themes/dead-simple/assets/js/app.js
@@ -7,12 +7,13 @@
 
     $document.ready(function () {
 
-        var $postContent = $(".post-content");
+        // var $postContent = $(".post-content");
         //$postContent.fitVids();
 
         //$(".scroll-down").arctic_scroll();
 
-        $("demoTabs").ds_tabs();
+        $(".ds-tabs").ds_tabs();
+        $(".ds-accordion").ds_accordions();
 
         $(".menu-button, .nav-cover, .nav-close").on("click", function (e) {
             e.preventDefault();
@@ -21,7 +22,7 @@
 
     });
 
-    $.fn.ds_tabs = function(options) {
+    $.fn.ds_tabs = function() {
 
         var tabContainer = $(this);
         var tabsNav = $(".tabs-nav > li > a");
@@ -38,8 +39,6 @@
             var tabId = $this.attr("data-tab");
             var tabHref = $this.attr("href").replace("#", "");
             var tabsContentId = tabsContent.attr("id");
-            //console.log("tabHref " + tabHref + " tabsContentId " + tabsContentId)
-
 
             if(!$this.hasClass("active")) {
                 tabsNav.removeClass("active");
@@ -49,13 +48,78 @@
             tabsContent.each(function() {
                 var id = $(this).attr("id");
                 if(id == tabHref) {
-                    //console.log("tabHref " + tabHref + " tabsContentId " + id);
                     var activeTab = $("#" + id);
                     tabsContent.removeClass("active").hide();
                     activeTab.addClass("active").show();
                 }
             });
         });
+    };
+
+    $.fn.ds_accordions = function() {
+        var $this = $(this);
+        var accordionTriggers = $this.find(".accordion-trigger");
+        var accordionPanels = $this.find(".accordion-panel");
+        var firstPanelHeight = accordionPanels.first()[0].scrollHeight;
+
+        accordionPanels.first().css({"max-height" : firstPanelHeight + "px"});
+
+        accordionTriggers.click(function(event) {
+            event.preventDefault();
+            var $this = $(this);
+            var triggerHref = $this.find("a").attr("href").replace("#", "");
+            accordionTriggers.removeClass("active");
+            accordionTriggers.attr("aria-expanded", false);
+            if($this.hasClass("active")) {
+                $this.removeClass("active");
+                $this.attr("aria-expanded", false);
+            }else {
+                $this.addClass("active");
+                $this.attr("aria-expanded", true);
+            }
+
+            accordionPanels.each(function() {
+                var id = $(this).attr("id");
+                if(id == triggerHref) {
+                    var activePanel = $("#" + id);
+                    accordionPanels.css({"max-height" : "0px"});
+                    if (activePanel.css("max-height") != "0px"){
+                        activePanel.css({"max-height" : 0});
+                    } else {
+                        activePanel.css({"max-height" : activePanel[0].scrollHeight + "px"});
+                    }
+                }
+            });
+        });
+    };
+
+    $.fn.ds_slideshow = function() {
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+        }
     }
 
     // Arctic Scroll by Paul Adam Davis
